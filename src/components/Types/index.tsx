d062bdde81532b36f1d33c481e27502757719f67
Jonathan Cheng <jcheng461@gmail.com>
2023-12-13 18:01:47 -0800

    add separated TS types to own directory & file

---
+++ src/components/Types/index.tsx
@@ -0,0 +1,57 @@
+import { json } from "react-router-dom";
+
+export type eTargetType = {
+  email: { value: string };
+  password: { value: string };
+};
+
+// Typing For Inventory Add Form
+export interface AddFormProps {
+  onFormSubmit: (data: InventoryEntry) => Promise<void>;
+  onClickingExit: () => void;
+}
+
+export interface InventoryEntry {
+  id?: string;
+  name: string;
+  description: string;
+  location: string;
+  checkedOut: boolean;
+  checkedOutBy: string | null;
+  dateCheckedOut: string | null;
+  tags: string[] | null;
+}
+
+interface CustomElements extends HTMLFormControlsCollection {
+  name: HTMLInputElement;
+  description: HTMLInputElement;
+  location: HTMLInputElement;
+  checkedOut: HTMLInputElement;
+  checkedOutBy: HTMLInputElement;
+  dateCheckedOut: HTMLInputElement;
+  tags: HTMLInputElement;
+}
+
+export interface CustomForm extends HTMLFormElement {
+  readonly elements: CustomElements;
+}
+
+// Typing for Inventory List component
+export type InventoryListProps = {
+  listOfEntries: InventoryEntry[];
+  onEntrySelection: (id: string) => void;
+  onClickingAddEntry: () => void;
+};
+
+// Typing for Inventory Entry component
+export type InventoryEntryProps = {
+  id?: string;
+  name: string;
+  description: string;
+  location: string;
+  checkedOut: boolean;
+  checkedOutBy: string | null;
+  dateCheckedOut: string | null;
+  tags: string[] | null;
+  onClickingDiv: (id: string) => void;
+};
