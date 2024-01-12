f2e73eaca30de95f07563216b4fbd04d46e74ff7
Jonathan Cheng <jcheng461@gmail.com>
2024-01-12 00:55:43 -0800

    WIP: add react-firebase-hooks/auth

--- src/helpers/UserContext.tsx
+++ src/helpers/UserContext.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { db, auth } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface UserInfo {
   name: string;
@@ -13,34 +15,27 @@ export const UserContext = React.createContext<UserInfo | null>(null);
 
 export const UserProvider = ({ children }: any) => {
   const [currentUser, setCurrentUser] = useState<UserInfo | null>(null);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
 
   useEffect(() => {
-    // Check if the user is authenticated. If logged in, to inventory. If not, to sign in
-    const checkAuth = async () => {
-      try {
-        if (auth.currentUser) {
-          const userRef = doc(db, "users", auth.currentUser.uid);
-          const docSnap = await getDoc(userRef);
-          if (docSnap.exists()) {
-            const userInfo = docSnap.data() as UserInfo;
-            setCurrentUser(userInfo);
-            // navigate("/inventory");
-          } else {
-            console.log("No such user in database. (User collections does not contain a user document with this id.)");
-          }
+    if (user) {
+      const fetchUserInfo = async () => {
+        const userRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+          const userInfo = docSnap.data() as UserInfo;
+          setCurrentUser(userInfo);
+          navigate("/inventory");
+        } else {
+          console.log("No such user in database.");
         }
-        // else if (localStorage !== null) {
-        //   navigate("/inventory");
-        // } else {
-        //   navigate("/signin");
-        // }
-      } catch (error) {
-        console.error("Error retrieving user info:", error);
-      }
-    };
-    checkAuth();
-  }, []);
+      };
+      fetchUserInfo();
+    } else {
+      console.log("User not authenticated.");
+    }
+  }, [user, navigate]);
 
   useEffect(() => {
     console.log("UserProvider, currentUser: ", currentUser);
@@ -94,3 +89,41 @@ export const UserProvider = ({ children }: any) => {
 
 //   return <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>;
 // };
+
+// useEffect(() => {
+//   // Check if the user is authenticated. If logged in, to inventory. If not, to sign in
+//   checkAuth();
+// }, []);
+
+// const checkAuth = async () => {
+//   const auth = getAuth();
+//   onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//       setCurrentUser(user);
+//     } else {
+//       console.log("What.");
+//     }
+//   });
+//   // previous checkAuth function contents go here.
+// };
+
+// try {
+//   if (auth.currentUser) {
+//     const userRef = doc(db, "users", auth.currentUser.uid);
+//     const docSnap = await getDoc(userRef);
+//     if (docSnap.exists()) {
+//       const userInfo = docSnap.data() as UserInfo;
+//       setCurrentUser(userInfo);
+//       // navigate("/inventory");
+//     } else {
+//       console.log("No such user in database. (User collections does not contain a user document with this id.)");
+//     }
+//   }
+//   // else if (localStorage !== null) {
+//   //   navigate("/inventory");
+//   // } else {
+//   //   navigate("/signin");
+//   // }
+// } catch (error) {
+//   console.error("Error retrieving user info:", error);
+// }
