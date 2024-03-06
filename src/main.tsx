8f119ca9c710b51268e54bb26e0284e369917185
Jon Cheng <joncheng.dev@gmail.com>
2024-03-05 23:41:40 -0800

    add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import { UserProvider } from "./helpers/UserContext.js";
+import { BrowserRouter } from "react-router-dom";
 // import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </UserProvider>
   </React.StrictMode>
 );
