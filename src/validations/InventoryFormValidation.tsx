43aa0af06384d749cd5e89ecab39ca665873cbec
Jon Cheng <joncheng.dev@gmail.com>
2024-03-06 12:43:03 -0800

    Updates

--- src/validations/InventoryFormValidation.tsx
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const formSchema = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
-  location: yup.string().required(),
-});
-
-// comment
