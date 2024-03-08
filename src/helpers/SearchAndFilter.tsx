a7cd6a78f3b4c9cb92a416b5691a86d6c132297e
Jon Cheng <joncheng.dev@gmail.com>
2024-03-08 09:53:03 -0800

    update filter to be exclusive rather than inclusive

--- src/helpers/SearchAndFilter.tsx
+++ src/helpers/SearchAndFilter.tsx
@@ -3,7 +3,7 @@ import { ItemType } from "../types";
 export const filterList = (list: ItemType[], searchQuery: string, tagsToFilter: string[]) => {
   let filteredListCopy = [...list];
   if (tagsToFilter.length > 0) {
-    filteredListCopy = filteredListCopy.filter((entry) => tagsToFilter.some((tag) => entry.tags.includes(tag)));
+    filteredListCopy = filteredListCopy.filter((entry) => tagsToFilter.every((tag) => entry.tags.includes(tag)));
   }
   if (searchQuery !== "") {
     filteredListCopy = filteredListCopy.filter((entry) => entry.displayName.toLowerCase().includes(searchQuery.toLowerCase()));
