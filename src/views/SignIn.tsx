dd459827fb09d1bdc99d50af18ff9c330c3ce160
Jon Cheng <joncheng.dev@gmail.com>
2024-05-20 15:02:26 -0700

    wip: fix google sign in button

--- src/views/SignIn.tsx
+++ src/views/SignIn.tsx
@@ -35,6 +35,7 @@ export default function SignIn() {
   const colors = tokens(theme.palette.mode);
   const userProvider = sharedInfo();
   const deploymentType = import.meta.env.VITE_REACT_APP_FIREBASE_DEPLOYMENT;
+  console.log("deploymentType: ", deploymentType);
   // state variables with useState hooks
   const [createAccountSuccess, setCreateAccountSuccess] = useState<string | null>(null);
   // const [signInSuccess, setSignInSuccess] = useState<boolean>(false);
