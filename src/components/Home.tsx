e7bbfb9f4e55723e52495ea97eddd9b6003d39ba
Jon Cheng <joncheng.dev@gmail.com>
2024-06-09 23:28:16 -0700

    remove unneeded lines

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,9 @@
-import { useState, useEffect } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 // Firebase / Firestore
 import { sharedInfo } from "../helpers/UserContext";
 // Components & Style
 import Layout from "./Layout.js";
 
-// type ContextType = { currentUser: useLocalStorageProps | null };
 interface UserInfo {
   email: string;
   isAdmin: boolean;
@@ -18,16 +16,6 @@ interface AuthContextValue {
 
 export default function Home() {
   const userProvider = sharedInfo();
-  const navigate = useNavigate();
-  const [isSidebarExpanded, setSidebarExpanded] = useState<boolean>(true);
-
-  useEffect(() => {
-    console.log("user: ", userProvider);
-  }, [userProvider]);
-
-  const handleSidebarToggle = () => {
-    setSidebarExpanded((prev) => !prev);
-  };
 
   return (
     <Layout>
